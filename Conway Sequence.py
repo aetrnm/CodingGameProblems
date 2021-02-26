7de42d7c679e6162789a269d67a6ce78cbf706f7
Oleg Gnat <banan20051@gmail.com>
2021-02-26 16:38:05 +0200

    Conway changed

--- Conway Sequence.py
+++ Conway Sequence.py
@@ -15,19 +15,13 @@ for i in range(n-2):
     curNum = line[0]
     counter = 0
     for j in range(len(line)):
-        try:
-            if(line[j] != curNum):
-                tempList.append(counter)
-                tempList.append(curNum)
-                curNum = line[j]
-                counter = 1
-            else:
-                counter += 1
-        except IndexError:
-            print("We are here at " + str(i))
+        if(line[j] != curNum):
             tempList.append(counter)
             tempList.append(curNum)
-            counter = 0
+            curNum = line[j]
+            counter = 1
+        else:
+            counter += 1
     tempList.append(counter)
     tempList.append(curNum)
     line = tempList.copy()
